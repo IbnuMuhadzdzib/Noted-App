ccd677919a9581b2068f4ff81713722d09db62a8
Consomeatsky <ibnulaginugas@gmail.com>
2025-10-17 11:12:35 +0700

    Fixed Home in Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
     );
   }
